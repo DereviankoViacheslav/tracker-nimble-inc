adad4b962b118120ed88cfeee0bff5716157ea2c
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-02-21 15:26:55 +0200

    add component App

---
+++ src/components/app/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import React from 'react';
+// import InputCreateTracker from '../inputCreateTracker';
+// import TrackerList from '../trackerList';
+
+function App() {
+  return (
+    <div className="tracker">
+      <h1 className="tracker__header">tracker</h1>
+      {/* <InputCreateTracker />
+      <TrackerList /> */}
+    </div>
+  );
+}
+
+export default App;
